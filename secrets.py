9a8d864c2b98a48d6a5413ab12eeabbf31586d97
meipipo <meteor1038@gmail.com>
2021-07-13 18:59:10 +0900

    delete secrets.py

--- secrets.py
+++
@@ -1,5 +0,0 @@
-# Go to https://apps.twitter.com/ and update your API keys below.
-consumer_key = "xxxxxxxxxxxxxxxxxxxxxxxxx"
-consumer_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-access_token = "xxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-access_token_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
