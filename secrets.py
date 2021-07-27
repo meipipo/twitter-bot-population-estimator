d036d07d52b43a47622553cddd11e5e8e2b31e4c
meipipo <meteor1038@gmail.com>
2021-07-27 18:30:32 +0900

    add secrets.py

---
+++ secrets.py
@@ -0,0 +1,8 @@
+# Go to https://apps.twitter.com/ and update your API keys below.
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+# Go to https://rapidapi.com/developer/dashboard app > security and paste the application key below.
+rapidapi_key = ""
